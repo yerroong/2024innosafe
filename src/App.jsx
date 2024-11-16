9e7f6ceef1a8e8c5c44769d5cb1357a3c675ec44
yerin <wbflqldks90@naver.com>
2024-11-16 15:38:38 +0900

    배포 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import Myhome from './pages/myhome';
